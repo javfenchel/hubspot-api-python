f32c766302b4b877da59709e09835bb125db7a3a
javfenchel <jeff@breatheforchange.com>
2024-08-29 12:06:40 -0500

    Update discovery.py
    This allows people to use public_object_api with tickets which will allow ticket merges

--- hubspot/discovery/crm/tickets/discovery.py
+++ hubspot/discovery/crm/tickets/discovery.py
@@ -9,6 +9,10 @@ class Discovery(DiscoveryBase):
         return self._configure_api_client(api_client, "BasicApi")
 
     @property
+    def public_object_api(self) -> api_client.PublicObjectApi:
+        return self._configure_api_client(api_client, "PublicObjectApi")
+
+    @property
     def search_api(self) -> api_client.SearchApi:
         return self._configure_api_client(api_client, "SearchApi")
 
